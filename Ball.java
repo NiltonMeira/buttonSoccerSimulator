113d2a61d5dea51949ada7a49afad801913e5025
Nilton <niltonprudys@gmail.com>
2024-05-20 21:51:23 -0300

    Creating the class World, and the class Ball

--- Ball.java
+++ Ball.java
@@ -1,10 +1,25 @@
-import java.awt.Point;
-
-import javax.swing.JComponent;
+import java.awt.*;
+import javax.swing.*;
 
 public class Ball extends JComponent 
 {
     private Point position;
-    private int radius;
+    private int radius = 20;
+
+    Ball()
+    {
+        this.position = new Point();
+        
+    }
+
+    public Point getPosition()
+    {
+        return position;
+    }
     
+
 }
+
+
+
+
